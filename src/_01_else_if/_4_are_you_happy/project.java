e18f55a91bec6f2cb6298699333424e319761901
league <league@imac-39.attlocal.net>
2024-01-26 16:57:04 -0800

    hi

--- src/_01_else_if/_4_are_you_happy/project.java
+++ src/_01_else_if/_4_are_you_happy/project.java
@@ -1,5 +1,18 @@
 package _01_else_if._4_are_you_happy;
 
 public class project {
-
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		fornitebattlepassssssssssss
+		I just crapped out my bottom
+		I like ortnite did I mention fortnite
+	}
 }
